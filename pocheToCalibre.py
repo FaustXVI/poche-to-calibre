850c92d65ed61db961b30ecc221c678da86b858f
Dmitry Sandalov <dmitry@sandalov.org>
2013-12-29 01:51:18 +0400

    all articles from home feed, comments, pep8

--- pocheToCalibre.py
+++ pocheToCalibre.py
@@ -1,10 +1,13 @@
+import re
 from calibre import strftime
 from calibre.web.feeds.recipes import BasicNewsRecipe
 
 
-class NYTimes(BasicNewsRecipe):
+class Poche(BasicNewsRecipe):
 
-    appURL = 'http://app.inthepoche.com'
+    app_url = 'http://app.inthepoche.com'
+    max_articles_per_feed = 15
+
     title = 'Poche'
     __author__ = 'Xavier Detant'
     description = 'Ma poche'
@@ -14,47 +17,84 @@ class NYTimes(BasicNewsRecipe):
 
     def get_browser(self):
         br = BasicNewsRecipe.get_browser(self)
+
+        # poche login
         if self.username and self.password:
-            if self.appURL == 'http://app.inthepoche.com':
-                br.open(self.appURL + '/u/' + self.username)
-            else:
-                br.open(self.appURL)
+            br.open(self.app_url + '/u/' + self.username) \
+                if self.app_url == 'http://app.inthepoche.com' \
+                else br.open(self.app_url)  # self-hosted poche
+
+            # submit the login form using credentials
             br.select_form(name='loginform')
             br['login'] = self.username
             br['password'] = self.password
             br.submit()
+
         return br
 
     def parse_index(self):
-        if self.appURL == 'http://app.inthepoche.com':
-            baseURL = self.appURL + '/u/' + self.username + '/'
-        else:
-            baseURL = self.appURL
-        soup = self.index_to_soup(baseURL + 'index.php')
+
+        base_url = self.app_url + '/u/' + self.username + '/' \
+            if self.app_url == 'http://app.inthepoche.com' \
+            else self.app_url  # self-hosted poche
+
+        # init values before loop
+        page_count = 1  # poche page iterator
+        articles_count = 0  # extracted articles counter
+        articles_number = 1  # total articles in poche library
+
         articles = {}
         key = None
         ans = []
 
-        for div in soup.findAll(True, attrs={'class': ['entrie']}):
+        # stop if no more articles or reached max articles limit
+        while ((articles_count < self.max_articles_per_feed) and
+               (articles_count < articles_number)):
 
-            a = div.find('a', href=True)
-            if not a:
-                continue
-            key = self.tag_to_string(div.find(
-                'a', attrs={'class': ['reading-time']}))
-            url = baseURL + a['href']
-            title = self.tag_to_string(a, use_alt=False)
-            description = ''
-            pubdate = strftime('%a, %d %b')
-            summary = div.find('p')
-            if summary:
-                description = self.tag_to_string(summary, use_alt=False)
+            # open poche on desired page
+            soup = self.index_to_soup(
+                base_url + "?view=home&sort=id&p=" + str(page_count))
 
-            feed = key if key is not None else 'Uncategorized'
-            if not feed in articles.keys():
-                articles[feed] = []
-            articles[feed].append(dict(
-                title=title, url=url, date=pubdate,
-                description=description, content=''))
+            # extract articles from entrie divs, articles num from nb-results
+            for div in soup.findAll(True, attrs={
+                    'class': ['entrie', 'nb-results']}):
+
+                # get total articles in poche lib
+                if div['class'] == 'nb-results':
+                    articles_number = int(re.findall(r'\d+', div.string)[0])
+
+                # continue if no article link
+                a = div.find('a', href=True)
+                if not a:
+                    continue
+
+                # use reading time as a key
+                key = self.tag_to_string(div.find(
+                    'a', attrs={'class': ['reading-time']}))
+
+                # extract article info
+                url = base_url + a['href']
+                title = self.tag_to_string(a, use_alt=False)
+                description = url
+                pubdate = strftime('%a, %d %b')
+                summary = div.find('p')
+                if summary:
+                    description = self.tag_to_string(summary, use_alt=False)
+                feed = key if key else 'Uncategorized'
+                if not feed in articles.keys():
+                    articles[feed] = []
+
+                # finally put articles to list
+                articles[feed].append(dict(
+                    title=title, url=url, date=pubdate,
+                    description=description, content=''))
+                articles_count += 1
+
+                # exit loop if max_articles_per_feed exceeded
+                if not ((articles_count < self.max_articles_per_feed) and
+                        (articles_count < articles_number)):
+                    break
+            page_count += 1
+
         ans = [(key, articles[key]) for key in articles.keys()]
         return ans
